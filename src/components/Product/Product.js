8dca6183347b948fccd1a9c47b08d60f353b3090
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-03-10 03:21:38 +0600

    Shopping Site

---
+++ src/components/Product/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Product = (props) => {
+
+
+    console.log(props.product);
+    return (
+        <div>
+            <h3>This Is Product</h3>
+           
+          
+
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+//import const [state, setstate] = useState(initialState)
+import {useState} from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+
+
+const Shop = () => {
+    const first10=fakeData.slice(0,10);
+    const [products,setProducts]=useState(first10);
+    //const first10=fake.slice(0,10);
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+            
+                {
+                 products.map(pd=><Product product={pd} ></Product>)
+
+                }
+           
+            </div>
+
+           <div className="cart-container">
+             <h3>This Is Cart</h3>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
